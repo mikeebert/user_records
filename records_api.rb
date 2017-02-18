6cd812848c1394dec6b6875f09848de1596d8d7f
Mike Ebert <mike@mikeebert.net>
2017-02-18 13:05:34 -0600

    Saves a record posted via API to Datastore

--- records_api.rb
+++ records_api.rb
@@ -1,4 +1,5 @@
 require 'grape'
+require 'datastore'
 
 module Records
   class API < Grape::API
@@ -7,8 +8,15 @@ module Records
       desc 'add a record to the datastore'
 
       post '/' do
+        record_creator = RecordCreator.new(request.body.string)
 
+        if record_creator.valid?
+          ::Datastore.save(record_creator.record)
+        else
+          status 400
+        end
       end
     end
+
   end
 end
--- spec/records_api_spec.rb
+++ spec/records_api_spec.rb
@@ -1,4 +1,6 @@
 require 'rack/test'
+require 'csv'
+require 'spec_helper'
 require './records_api'
 
 def app
@@ -8,10 +10,31 @@ end
 describe Records::API do
   include Rack::Test::Methods
 
-  it 'posts to a records endpoint' do
-    post '/records'
-
-    expect(last_response.status).to eq(201)
+  before(:all) do
+    Datastore.set_location('./spec/support/record_datastore.txt')
   end
+
+  after(:each) do
+    clear_datastore
+  end
+
+  context 'records' do
+
+    context 'no data posted' do
+      it 'returns a 400 if no data is sent' do
+        post '/records'
+
+        expect(last_response.status).to eq(400)
+      end
+    end
+  end
+
+  it 'saves a record do to the datastore' do
+    data = "ApiTestLast, TestFirst, TestColor, 01/01/01"
+    post '/records', data, 'CONTENT_TYPE' => 'application/txt'
+
+    expect(Datastore.records.first.last_name).to eq('ApiTestLast')
+  end
+
 end
 
