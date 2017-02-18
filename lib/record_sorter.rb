5bcb356457b39e55ca37451f38273fd6ebe439a2
Mike Ebert <mike@mikeebert.net>
2017-02-18 10:10:34 -0600

    Adds naive RecordSorter

---
+++ lib/record_sorter.rb
@@ -0,0 +1,16 @@
+class RecordSorter
+  def self.sort_by_last_name_descending(records)
+    records.sort_by { |r| r.last_name }.reverse
+  end
+
+  def self.sort_by_birth_date(records)
+    records.sort_by { |r| r.date_of_birth }
+  end
+
+  def self.sort_by_color_and_last_name(records)
+    records.sort do |a, b|
+      comparison = (b.favorite_color <=> a.favorite_color)
+      comparison.zero? ? (a.last_name <=> b.last_name) : comparison
+    end
+  end
+end
