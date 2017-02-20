bd1d5ecc99dbe0d57aa9927be07ecf3650cbcf86
Mike Ebert <mike@mikeebert.net>
2017-02-20 07:02:35 -0800

    Returns UserRecord attributes as hash

--- lib/user_record.rb
+++ lib/user_record.rb
@@ -1,3 +1,5 @@
+require 'grape-entity'
+
 class UserRecord
 
   attr_reader :last_name,
@@ -10,5 +12,14 @@ class UserRecord
     @first_name     = attributes[:first_name]
     @favorite_color = attributes[:favorite_color]
     @date_of_birth  = attributes[:date_of_birth]
+  end
+
+  def attributes
+    {
+      last_name: @last_name,
+      first_name: @first_name,
+      favorite_color: @favorite_color,
+      date_of_birth: @date_of_birth
+    }
   end
 end
--- spec/user_record_spec.rb
+++ spec/user_record_spec.rb
@@ -8,18 +8,30 @@ describe UserRecord do
   end
 
   it 'has a first name' do
-    record = UserRecord.new({first_name: "First"})
-    expect(record.first_name).to eq("First")
+    record = UserRecord.new({first_name: 'First'})
+    expect(record.first_name).to eq('First')
   end
 
   it 'has a favorite color' do
-    record = UserRecord.new({favorite_color: "Blue"})
-    expect(record.favorite_color).to eq("Blue")
+    record = UserRecord.new({favorite_color: 'Blue'})
+    expect(record.favorite_color).to eq('Blue')
   end
 
   it 'has a date of birth' do
-    date_of_birth = Date.strptime("01/01/2012","%m/%d/%Y")
+    date_of_birth = Date.strptime('01/01/2012','%m/%d/%Y')
     record = UserRecord.new({date_of_birth: date_of_birth})
     expect(record.date_of_birth).to eq(date_of_birth)
+  end
+
+  it 'returns the attrubutes as a hash' do
+    attributes = {
+      first_name: 'Roger',
+      last_name: 'Rabbit',
+      date_of_birth: Date.parse('01/01/01'),
+      favorite_color: 'yellow'
+    }
+    record = UserRecord.new(attributes)
+
+    expect(record.attributes).to eq(attributes)
   end
 end
